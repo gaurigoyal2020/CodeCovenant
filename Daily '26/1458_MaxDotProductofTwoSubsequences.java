de13560cd9840bad24f0716c55e9f5977ec2706b
Gauri Goyal <1000018487@dit.edu.in>
2026-01-11 00:02:28 +0530

    Implement max product of two subsequences in tree

---
+++ Daily '26/1458_MaxDotProductofTwoSubsequences.java
@@ -0,0 +1,38 @@
+class Solution {
+    static final int MOD = 1_000_000_007;
+        long totalSum = 0;
+        long maxProduct = 0;
+
+        //Total tree Sum
+        long getTotalSum(TreeNode root) {
+            if(root == null)
+                return 0;
+            return root.val
+                + getTotalSum(root.left)
+                + getTotalSum(root.right);
+        }
+
+        //Subtree sum : postorder dfs
+        long dfs(TreeNode root) {
+            if(root == null)
+                return 0;
+
+            //Sum of riht and left subtrees    
+            long left = dfs(root.left);
+            long right = dfs(root.right);
+
+            //current subtree sum
+            long subSum = root.val + left + right;
+
+            long product = subSum * (totalSum - subSum);
+            maxProduct = Math.max(maxProduct, product);
+            return subSum;
+        }
+
+    public int maxProduct(TreeNode root) {
+        totalSum = getTotalSum(root);
+        dfs(root);
+        return (int)(maxProduct % MOD);
+
+    }
+}
