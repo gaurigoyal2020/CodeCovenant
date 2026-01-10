47af9b4da6521cd917d126a4570007cee1eaf8d7
Gauri Goyal <1000018487@dit.edu.in>
2026-01-11 00:12:54 +0530

    Create 865_SmallestSubtreewithalltheDeepestNodes.java

---
+++ Daily '26/865_SmallestSubtreewithalltheDeepestNodes.java
@@ -0,0 +1,29 @@
+class Solution {
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        return dfs(root).node;
+    }
+    public Result dfs(TreeNode node) {
+        if(node == null)
+            return new Result(null, 0);
+
+        Result L = dfs(node.left);
+        Result R = dfs(node.right);
+
+        if(L.dist > R.dist)
+            return new Result(L.node, L.dist+1);
+        if(R.dist > L.dist)
+            return new Result(R.node, R.dist+1);
+        
+        return new Result(node, L.dist+1);
+    }
+}
+
+class Result {
+
+    TreeNode node;
+    int dist;
+    Result(TreeNode n, int d) {
+        node = n;
+        dist = d;
+    }
+}
